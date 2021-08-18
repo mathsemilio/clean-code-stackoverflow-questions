32c48dc90940c131c015906ada62aa9086e4820d
mathsemilio <mathsemilio@gmail.com>
2021-08-18 18:07:50 -0300

    Adição de dependências básicas para o desenvolvimento do projeto; Configuração do AndroidManifest; Exclusão de arquivos desnecessários; Alteração de arquivos da IDE para alteração do nome do projeto.

--- app/src/main/java/com/github/mathsemilio/stackoverflowquestions/MainActivity.kt
+++ app/src/main/java/com/github/mathsemilio/stackoverflowquestions/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.github.mathsemilio.stackoverfowquestions
+package com.github.mathsemilio.stackoverflowquestions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
