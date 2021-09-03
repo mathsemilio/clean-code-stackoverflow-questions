96eb7e383ef0ac22d49a4886c82be4a6925362fc
mathsemilio <mathsemilio@gmail.com>
2021-09-03 15:29:40 -0300

    Constante para Chave de API extraída

--- app/src/main/java/com/github/mathsemilio/stackoverflowquestions/common/Constants.kt
+++ app/src/main/java/com/github/mathsemilio/stackoverflowquestions/common/Constants.kt
@@ -1,3 +1,5 @@
 package com.github.mathsemilio.stackoverflowquestions.common
 
+const val API_KEY = "UvCy98LL0Z9RRyxlla47cg(("
+
 const val ARG_QUESTION_ID = "question_id"
--- app/src/main/java/com/github/mathsemilio/stackoverflowquestions/networking/api/StackoverflowApi.kt
+++ app/src/main/java/com/github/mathsemilio/stackoverflowquestions/networking/api/StackoverflowApi.kt
@@ -1,5 +1,6 @@
 package com.github.mathsemilio.stackoverflowquestions.networking.api
 
+import com.github.mathsemilio.stackoverflowquestions.common.API_KEY
 import com.github.mathsemilio.stackoverflowquestions.domain.model.response.question.QuestionsResponseSchema
 import com.github.mathsemilio.stackoverflowquestions.domain.model.response.questiondetail.QuestionDetailsResponseSchema
 import retrofit2.Response
@@ -8,10 +9,10 @@ import retrofit2.http.Path
 
 interface StackoverflowApi {
 
-    @GET("questions?key=UvCy98LL0Z9RRyxlla47cg((&pagesize=30&order=desc&sort=activity&site=stackoverflow")
+    @GET("questions?key=$API_KEY&pagesize=30&order=desc&sort=activity&site=stackoverflow")
     suspend fun fetchLastActiveQuestions(): Response<QuestionsResponseSchema>
 
-    @GET("questions/{questionId}?key=UvCy98LL0Z9RRyxlla47cg((&site=stackoverflow&filter=withbody")
+    @GET("questions/{questionId}?key=$API_KEY&site=stackoverflow&filter=withbody")
     suspend fun fetchQuestionDetails(
         @Path("questionId") questionId: String
     ): Response<QuestionDetailsResponseSchema>
